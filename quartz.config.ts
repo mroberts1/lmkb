b3a046834fe11d60ec3a57d1531c3887d52d6f40
Martin Roberts <mroberts1@gmail.com>
2026-04-06 23:41:34 -0400

    Rename site to lmkb

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,13 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "lm-wiki",
+    pageTitle: "lmkb",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "mroberts1.github.io/lm-wiki",
+    baseUrl: "mroberts1.github.io/lmkb",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
